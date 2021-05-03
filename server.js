00739ec723174b7f24ffbdb0439e81daca552754
Alfredo Tlahuel <Vfredo602@gmail.com>
2021-05-03 18:38:45 -0500

    added routes to server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const api = require('./routes/apiRoutes');
+const html = require('./routes/htmlRoutes');
 
 // starts the app and creates a port
 const app = express();
@@ -7,7 +9,9 @@ const PORT = 8080;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'))
-
+// routes
+app.use('/api', api);
+app.use('/', html);
 
 // const server = http.createServer(handleRequest);
 
