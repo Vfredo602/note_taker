2ccc8ec847d63f2198625badb8d2246d01067db2
Alfredo Tlahuel <Vfredo602@gmail.com>
2021-05-10 14:59:36 -0500

    new port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const html = require('./routes/htmlRoutes');
 
 // starts the app and creates a port
 const app = express();
-const PORT = 8080;
+// const PORT = 8080;
 var PORT = process.env.PORT || 3001;
 
 // middleware parsing into json
